9d46306da592bc42552e683dfa9c99c20a0c0823
natalie.seeto <nataliejseeto@gmail.com>
2020-04-21 15:06:37 +0100

    setup, jest and test cases for first kata

---
+++ merge-meeting-times/solution.js
@@ -0,0 +1,8 @@
+const mergeRanges = (array) => {
+    let mergedTimes = []
+    const lastElement = array[array.length - 1]
+
+    return array
+}
+
+module.exports = mergeRanges
---
+++ merge-meeting-times/test.js
@@ -0,0 +1,48 @@
+const mergeRanges = require('./solution.js')
+
+test('when given one object in the array, it should return that range', () => {
+    const arr = [
+        { startTime: 0, endTime: 1}
+    ]
+    expect(mergeRanges(arr)).toBe(arr)
+})
+
+test('when given two objects in the array, it should return two ranges', () => {
+    const arr = [
+        { startTime: 0, endTime: 1},
+        { startTime: 3, endTime: 5}
+    ]
+
+    const result = [
+        { startTime: 0, endTime: 1},
+        { startTime: 3, endTime: 5}
+    ]
+    expect(mergeRanges(arr)).toBe(result)
+})
+
+test('when given two objects in the array, it should return single range with increased end time', () => {
+    const arr = [
+        { startTime: 0, endTime: 1},
+        { startTime: 0, endTime: 3}
+    ]
+
+    const result = [
+        { startTime: 0, endTime: 3}
+    ]
+    expect(mergeRanges(arr)).toBe(result)
+})
+
+test('when given two objects in the array, it should return single range with increased start time', () => {
+    const arr = [
+        { startTime: 5, endTime: 10},
+        { startTime: 3, endTime: 10}
+    ]
+
+    const result = [
+        { startTime: 3, endTime: 10}
+    ]
+    expect(mergeRanges(arr)).toBe(result)
+})
+
+
+// other test cases
