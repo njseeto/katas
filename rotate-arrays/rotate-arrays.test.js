f927d3c0241e091eacd820697e66b80b676fc0be
natalie.seeto <nataliejseeto@gmail.com>
2020-05-07 14:07:55 +0100

    rotate-arrays: solution, bit hacky

--- rotate-arrays/rotate-arrays.test.js
+++ rotate-arrays/rotate-arrays.test.js
@@ -1,1 +1,28 @@
 const rotate = require('./solution')
+
+it('array moves one place when k = 1', () => {
+    let arr = [ 1, 2, 3 ]
+    let k = 1
+
+    expect(rotate(arr, k)).toEqual([ 2, 3 ,1 ])
+})
+
+it('when k is less than 0, an error message appears', () => {
+    let arr = [ 1, 2, 3 ]
+    let k = -1
+
+    expect(rotate(arr, k)).toEqual("K must be a greater than or equal to 0")
+})
+
+// test to move 3 places
+it('array moves two places when k = 3', () => {
+    let arr = [ 1, 2, 3, 4, 5, 6, 7 ]
+    let k = 3
+
+    expect(rotate(arr, k)).toEqual([ 5, 6, 7, 1, 2, 3, 4 ])
+})
+
+// test to move 5 places
+
+
+// test to move 10 places
--- rotate-arrays/solution.js
+++ rotate-arrays/solution.js
@@ -1,5 +1,19 @@
 const rotate = (arr, k) => {
-    
+    if(k === 1){
+        for(let i = 1; i <= k; i++){
+            arr.push(arr.splice(0, 1)[0])
+        }
+        return arr
+    } else if (k > 1){
+        for(let i = 0; i <= k; i++){
+            arr.push(arr.splice(0, 1)[0])
+        }
+        return arr
+    }
+    else {
+        let error = "K must be a greater than or equal to 0"
+        return error
+    }
 }
 
 module.exports = rotate
