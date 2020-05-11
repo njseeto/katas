953b57b17681bf2013eee17a783f485dd251525f
natalie.seeto <nataliejseeto@gmail.com>
2020-05-11 15:00:03 +0100

    cake shop: add solution

--- cake-shop/cake-shop.test.js
+++ cake-shop/cake-shop.test.js
@@ -1,17 +1,18 @@
 const serviceChecker = require('./solution')
 
 // TEST CASES
-test('when given orders from take out array, should return true if servedOrder is ordered', () => {
-    const takeOutOrders = [ 5, 6, 7]
-    const dineInOrders = [ 2 ]
-
-    const servedOrders = [ 5, 6, 7, 2]
+test('should return FALSE if servedOrder is not first come first serve', () => {
+    const takeOutOrders = [1, 3, 5]
+    const dineInOrders = [ 2, 4, 6 ]
+    const servedOrders = [ 1, 2, 4, 6, 5, 3 ]
 
     expect(serviceChecker(takeOutOrders, dineInOrders, servedOrders)).toBeFalsy()
 })
 
+test('should return TRUE if servedOrder is first come first serve', () => {
+    const takeOutOrders = [17, 8, 24]
+    const dineInOrders = [ 12, 19, 2 ]
+    const servedOrders = [ 17, 8, 12, 19, 24, 2]
 
-// takeOut = [ 1, 3, 5 ] dineIn = [ 2, 4, 6 ]; served order = [ 1, 2, 4, 6, 5, 3 ] is false
-
-
-// takeOut = [ 17, 8, 24 ] dineIn = [ 12, 19, 2 ]; served order = [ 17, 8, 12, 19, 24, 2 ] is true
+    expect(serviceChecker(takeOutOrders, dineInOrders, servedOrders)).toBeTruthy()
+})
--- cake-shop/solution.js
+++ cake-shop/solution.js
@@ -3,10 +3,23 @@
 // Function --> input 3x arrays, output boolean
 
 const serviceChecker = ( takeOutOrders, dineInOrders, servedOrders ) => {
-    
-    // use indexOf() method
-    // clone the arrays, replace the elements in the takeOut and dineIn arrays with indices.
-    // map the number to the index
-    // check the servedOrder array for the corresponding index and check if the left number is less than or equal to the right
-    
+
+    // Base case is when the servedOrders array has no more orders in it
+    if(servedOrders.length == 0){
+        return true
+    } 
+
+    // Check if there are any takeOutOrders and if the first matches the first from the servedOrders
+    if (takeOutOrders.length && takeOutOrders[0] === servedOrders[0]){
+
+        // Calls the method, removing the first element in the array
+        return serviceChecker(takeOutOrders.splice(1), dineInOrders, servedOrders.splice(1))
+
+    } else if (dineInOrders.length && dineInOrders[0] === servedOrders[0]){
+        // does the same logic here but with the dinerInOrders
+        return serviceChecker(takeOutOrders, dineInOrders.splice(1), servedOrders.splice(1))
+
+    } else return false
 }
+
+module.exports = serviceChecker
